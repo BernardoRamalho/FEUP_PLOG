f86c3d1f8fc1efff8eb3e38e357cada772f947e8
Bernardo Ramalho <49244133+BernardoRamalho@users.noreply.github.com>
2020-12-29 23:05:54 +0000

    Added test data

---
+++ TP2/data.pl
@@ -0,0 +1,28 @@
+% house(id, proprietário)
+
+house(1, 'Manel').
+house(2, 'Joana').
+house(3, 'Antunes').
+house(4, 'José').
+
+% tempoViagem(local, local, tempo)
+
+tempoViagem('padaria', 1, 5).
+tempoViagem('padaria', 2, 15).
+tempoViagem('padaria', 3, 25).
+tempoViagem('padaria', 4, 1).
+tempoViagem(1, 2, 3).
+tempoViagem(1, 3, 14).
+tempoViagem(1, 4, 20).
+
+tempoViagem(2, 1, 3).
+tempoViagem(2, 3, 10).
+tempoViagem(2, 4, 5).
+
+tempoViagem(3, 2, 10).
+tempoViagem(3, 1, 14).
+tempoViagem(3, 4, 8).
+
+tempoViagem(4, 2, 5).
+tempoViagem(4, 3, 8).
+tempoViagem(4, 1, 20).
