23bf1268ae8fcd3d3d701c3ff144607be5f7f3aa
Bernardo Ramalho <49244133+BernardoRamalho@users.noreply.github.com>
2021-01-04 21:38:06 +0000

    ultra feito

--- TP2/main.pl
+++ TP2/main.pl
@@ -168,5 +168,4 @@ displayTableContent([House|Rest], [Time|RestTime]):-
 reset_timer :- statistics(walltime,_).	
 print_time :-
 	statistics(walltime,[_,T]),
-	TS is ((T/10)*10)/1000,
 	nl, write('Time: '), write(T), write('ms'), nl, nl.
